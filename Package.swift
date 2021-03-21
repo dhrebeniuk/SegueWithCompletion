4caf7a3f9cc30d41373b9843f58ca08ee13f0e3b
Dmytro Hrebeniuk <dmytrohrebeniuk@gmail.com>
2021-03-21 13:36:13 +0200

    Remove files from Package

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ let package = Package(name: "SegueWithCompletion",
                       platforms: [.iOS(.v8)],
                       products: [.library(name: "SegueWithCompletion",
                                           targets: ["SegueWithCompletion"])],
-                      targets: [.target(name: "SegueWithCompletion",
-                                        path: ".")],
+                      targets: [
+                            .target(name: "SegueWithCompletionExample",
+                            path: ".",
+                            exclude: ["AppDelegate.swift", "ModalScreenViewController.swift", "MainScreenViewController.swift", "DetailsScreenViewController.swift", "ModelObject.swift", "Main.storyboard", "LaunchScreen.storyboard", "Info.plist"])
+                      ],
                       swiftLanguageVersions: [.v5])
                       
